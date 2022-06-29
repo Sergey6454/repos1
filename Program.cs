47032e8bb7853d5d1284df78dbeb0d862918d9e3
Sergei <SergeiEmail>
2022-06-29 18:28:40 +0500

    Proverka

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int a = 0, b = 0;
-
+             
             while(true)
             {
                 Console.WriteLine("1-Ввести А\n" +
